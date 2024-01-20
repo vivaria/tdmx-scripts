ba723ddce576a61762dc6dc6ca152baef7e8f2ce
Viv <ms.vivaria@gmail.com>
2024-01-20 12:37:28 -0500

    xb1: try and fail to snatch official bins

--- BIN_move_xb1_bins_into_folders.py
+++ BIN_move_xb1_bins_into_folders.py
@@ -1,10 +1,20 @@
 import os
 import shutil
+from pathlib import Path
 
 CUSTOM_SONG_DIR = os.path.join("D:\\", "games", "TaikoTDM",
                                "CustomSongSources", "XB1")
 for root, dirs, files in os.walk(CUSTOM_SONG_DIR, topdown=True):
-    for dir_name in dirs:
-        filename = f"song_{dir_name}.bin"
-        shutil.move(os.path.join(root, filename),
-                    os.path.join(root, dir_name, filename))
+    old_id = Path(root).name
+    new_id = f"xb1_{old_id}"
+    if f"song_{old_id}.bin" not in files:
+        continue
+    for fname in files:
+        if "xb1" in fname:
+            continue
+        old_fpath = os.path.join(root, fname)
+        new_fpath = os.path.join(root, fname.replace(old_id, new_id))
+        shutil.move(old_fpath, new_fpath)
+    if "xb1" not in root:
+        os.rename(os.path.join(CUSTOM_SONG_DIR, old_id),
+                  os.path.join(CUSTOM_SONG_DIR, new_id))
