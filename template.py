06087d2660d89c1fb3e334efe0ee7f2ae1d9831d
Viv <ms.vivaria@gmail.com>
2024-03-09 17:48:51 -0500

    Add template file for new scripts

---
+++ template.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+A simple python script template.
+"""
+
+# stdlib
+import sys
+
+# third party libraries
+
+
+def main():
+    breakpoint()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
